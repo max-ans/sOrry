88e037529579e5d302b8ae8d05623d4bad32f8fd
max-ans <maxenceroyer@hotmail.com>
2020-09-24 16:50:02 +0200

    First complete fixtures

--- back/src/DataFixtures/AppFixtures.php
+++ back/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Apology;
+use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\User;
 use App\Service\Slugger;
@@ -39,7 +40,31 @@ class AppFixtures extends Fixture
             'Yo si toi aussi tu aime coder alors viens me suivre sur ma chaine YT'
         ];
         
-        
+        $categoriesTitles = [
+            'Arrestation',
+            'Boulot',
+            'Famille',
+            'Jeux vidéo',
+            'Ventes',
+            'Retard',
+            'Arrestation',
+        ];
+
+        $categories = [];
+
+              
+        for ($categoriesIndex = 0 ; $categoriesIndex < count($categoriesTitles); $categoriesIndex++) {
+
+            $category = new Category;
+            $category->setTitle($categoriesTitles[$categoriesIndex]);
+            $category->setSlug($this->slugger->slugify($category->getTitle()));
+            $category->setDescription($faker->realText());
+            
+            $categories[] = $category;
+
+            $manager->persist($category);
+        }
+
         
         for($userIndex = 0; $userIndex <= 10 ; $userIndex++) {
             $user = new User;
@@ -48,8 +73,10 @@ class AppFixtures extends Fixture
             $user->setRoles(['ROLE_USERS']);
             $user->setEmail($user->getFirstname() . $user->getLastname() . '@gmail.com');
             $user->setPassword($this->passwordEncoder->encodePassword($user , $user->getFirstname()));
-            
-            for ($apologyIndex = 0; $apologyIndex <= mt_rand(0 , 5) ; $apologyIndex++ ) {
+            $user->setNickname(substr($user->getFirstname(), 0, 3));
+      
+
+            for ($apologyIndex = 0; $apologyIndex <= mt_rand(0 , 3) ; $apologyIndex++ ) {
                 $apology = new Apology;
                 $apology->setAuthor($user);
                 $apology->setContent($faker->realText());
@@ -57,6 +84,17 @@ class AppFixtures extends Fixture
                 $apology->setSlug($this->slugger->slugify($apology->getTitle()));
                 $apology->setLikes($faker->numberBetween(2, 5000));
                 $apology->setCreatedAt(new DateTime());
+                $apology->addCategory($categories[mt_rand(0, 5)]);
+
+                for ($commentIndex = 0 ; $commentIndex <= 2 ; $commentIndex++){
+                  $comment = new Comment;
+                  $comment->setContent($fakeComments[mt_rand(0, 5)]);
+                  $comment->setApology($apology);
+                  $comment->setAuthor($user); 
+
+                  $manager->persist($comment);
+                }
+
 
                 $manager->persist($apology);
             }
