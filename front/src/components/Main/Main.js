80b9c6f0ae94e9697292b59cd58fe36b05269068
Max-ans <maxenceroyer@hotmail.com>
2020-10-06 17:46:05 +0200

    create Main component

---
+++ front/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './main.scss';
+
+const Main = () => (
+  <main className="main">
+    <h2 className="main-title">
+      Les meilleurs excuses
+    </h2>
+  </main>
+);
+
+export default Main;
