71cb3de65f22ec3f62eac0db1f666c5145c5fbe8
max-ans <maxenceroyer@hotmail.com>
2020-10-09 14:16:36 +0200

    New responsive design for Main componant and new app background

---
+++ front/src/components/Description/Description.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './description.scss';
+
+const Description = () => (
+  <section className="description">
+    <h2 className="description-title">
+      <span className="description-title-span">sorry</span>
+      ,&nbsp;mais pourquoi ?
+    </h2>
+    <p className="description-content">
+      Combien de fois vous est-il arrivé d'être en retard au travail,
+      repas de famille ou tout simplement un rendez-vous ?
+      Et combien de fois avez-vous dû chercher des excuses,
+      toutes aussi foireuses les une que les autres ?
+      sOrry est un solution qui vous permettra de ne plus avoir à
+      chercher pendant de longues minutes un excuses de retard (valable... ou pas)
+      pour vous justifier.
+      Cette solution se veut humoristique et n'as pas pour but de promouvoir le retard!
+      Son but est simplement de vous faire passer un bon moment,
+      en vous faisant lire les perles des excuses de retards.
+      Et qui sait ?! Ça vous sauveras peut être les miches.... ou pas.
+    </p>
+  </section>
+);
+
+export default Description;
--- front/src/components/Main/Main.js
+++ front/src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Carousel from 'src/containers/Carousel/Carousel';
+import Description from 'src/components/Description/Description';
 
 import './main.scss';
 
@@ -10,6 +11,7 @@ const Main = () => (
       les meilleurs excuses
     </h2>
     <Carousel />
+    <Description />
   </main>
 );
 
