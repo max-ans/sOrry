17875c27f03394ecdcc95ce62a1a87f0adf9f633
max-ans <maxenceroyer@hotmail.com>
2020-10-23 15:44:10 +0200

    Link to home on site title

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Menu } from 'react-feather';
+import { Link } from 'react-router-dom';
 import className from 'classnames';
 
 import Nav from 'src/containers/Nav/Nav';
@@ -10,9 +11,11 @@ import './header.scss';
 const Header = ({ setMenuIsOpen, menuIsOpen }) => (
   <header className="header">
     <div className="header-banner">
-      <h1 className="header-title">
-        s'Orry
-      </h1>
+      <Link to="/">
+        <h1 className="header-title">
+          s'Orry
+        </h1>
+      </Link>
       <Menu
         className={className('header-buttonMenu ', { 'isActive ': menuIsOpen })}
         onClick={setMenuIsOpen}
