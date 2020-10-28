3ab443b89cf6c9c3fdce4e8521e60c31276e4103
max-ans <maxenceroyer@hotmail.com>
2020-10-28 18:18:48 +0100

    Email sending test

--- back/src/Controller/Api/v0/ContactController.php
+++ back/src/Controller/Api/v0/ContactController.php
@@ -2,18 +2,53 @@
 
 namespace App\Controller\Api\v0;
 
+use App\Form\ContactType;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/api/v0/contact", name="api_v0_contact")
+     * @Route("/api/v0/contact", name="api_v0_contact", methods={"POST"})
      */
-    public function contact()
+    public function contact(Request $request, Swift_Mailer $mailer)
     {
+        $requestSent = json_decode($request->getContent(), true);
+    
+        
+        $form = $this->createForm(ContactType::class, null, ['csrf_protection' => false]);
+
+        $form->submit($requestSent);
+
+        if ($form->isValid()) {
+
+            $message = new \Swift_Message("s'Orry");
+            $message->setFrom($requestSent['sendFrom']);
+            $message->setTo('royer.maxence.dev@gmail.com');
+            $message->setBody(
+                'email/contact.html.twig',
+                [
+                    'message' => $requestSent['message'],
+                    'authorization' => $requestSent['responseAccept']
+                ], 
+               
+            );
+
+            $mailer->send($message);
+
+
+            return $this->json([
+                'message' => "Your request has been send",
+            ], 200);
+        }
+
+        
         return $this->json([
             'message' => "Your request has been send",
-        ], 200);
+            'reasons' => (string) $form->getErrors(true, true),
+        ], 400);
+        
     }
 }
