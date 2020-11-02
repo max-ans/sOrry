4dad32aabd16e6e6fff1bd06eacc0f77603634bc
max-ans <maxenceroyer@hotmail.com>
2020-11-02 13:57:11 +0100

    Style for Contact componant

--- front/src/components/Contact/Contact.js
+++ front/src/components/Contact/Contact.js
@@ -4,15 +4,26 @@ import './contact.scss';
 
 const Contact = () => (
   <div className="contact">
-    <h2 className="contact title">Contact</h2>
-    <p className="contact-paragraphe">
-      Vous souhaitez nous faire part d'une remarque,
-      d'un bug ou tout simplement échanger ?
-      N'hésitez pas! Ce formulaire est fait pour vous :wink:
-    </p>
+    <div className="contact-header">
+
+      <p className="contact-paragraphe">
+        Vous souhaitez nous faire part d'une remarque,
+        d'un bug ou tout simplement donner votre avis ?
+        N'hésitez pas! Ce formulaire est fait pour vous.
+        <span
+          role="img"
+          aria-label="clin d'oeil"
+          className="paragraphe-emoji"
+        >
+          😉
+        </span>
+      </p>
+    </div>
     <form action="" className="contact-form">
       <fieldset className="form-fieldset">
+        <legend className="form-legend">Contact</legend>
         <label htmlFor="email" className="form-label">
+          Votre adresse email :
           <input
             type="email"
             className="form-input"
@@ -21,14 +32,17 @@ const Contact = () => (
           />
         </label>
         <label htmlFor="text" className="form-label">
+          Votre message :
           <textarea
+            rows="10"
             type="textarea"
             className="form-input"
             id="text"
             name="text"
           />
         </label>
-        <label htmlFor="checkbox" className="form-label">
+        <label htmlFor="checkbox" className="form-label checkbox">
+          J'accepte d'être contacté par l'équipe :
           <input
             type="checkbox"
             className="form-input"
@@ -36,6 +50,7 @@ const Contact = () => (
             name="checkbox"
           />
         </label>
+        <button className="form-submit" type="submit">Envoyer</button>
       </fieldset>
     </form>
   </div>
