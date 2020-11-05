3e6ce5b673875252a32086dfb2ce1bbf45f88146
max-ans <maxenceroyer@hotmail.com>
2020-11-05 15:49:24 +0100

    Form for apology creation without style

--- front/src/components/ApologyCreate/ApologyCreate.js
+++ front/src/components/ApologyCreate/ApologyCreate.js
@@ -4,7 +4,41 @@ import './apologyCreate.scss';
 
 const ApologyCreate = () => (
   <div className="apology-create">
-    apologycreate
+    <form
+      action=""
+      className="apology-form"
+    >
+      <fieldset
+        className="form-fieldset"
+      >
+        <legend className="form-legend">Poster une excuse</legend>
+        <label htmlFor="title" className="form-label">
+          Titre de l'excuse :
+          <input
+            type="text"
+            className="form-input"
+            id="title"
+            name="title"
+          />
+        </label>
+        <label htmlFor="apology" className="form-label">
+          L'excuse :
+          <textarea
+            rows="10"
+            type="textarea"
+            className="form-input"
+            id="apology"
+            name="apology"
+          />
+        </label>
+        <button
+          type="submit"
+          className="form-submit"
+        >
+          Poster
+        </button>
+      </fieldset>
+    </form>
   </div>
 );
 
