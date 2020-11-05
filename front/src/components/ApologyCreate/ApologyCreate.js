2ea3fc545608841333b5eb39047adbd6242717a4
max-ans <maxenceroyer@hotmail.com>
2020-11-05 15:36:04 +0100

    ApologyCreate component and new link for him in ProfilPage component

---
+++ front/src/components/ApologyCreate/ApologyCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './apologyCreate.scss';
+
+const ApologyCreate = () => (
+  <div className="apology-create">
+    apologycreate
+  </div>
+);
+
+export default ApologyCreate;
