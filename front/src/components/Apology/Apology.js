bb3da6b519f2521a234f14966e5312291411b914
Max-ans <maxenceroyer@hotmail.com>
2020-12-16 11:59:43 +0100

    Design for Apology component V1 + recovery id for comments

--- front/src/components/Apology/Apology.js
+++ front/src/components/Apology/Apology.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Loader from 'src/components/Loader/Loader';
-import { ThumbsUp } from 'react-feather';
+import { ThumbsUp, MessageSquare } from 'react-feather';
 
 import './apology.scss';
 
@@ -33,26 +33,48 @@ const Apology = ({
               <p className="apology-date">
                 {apologyInformation.author.nickname} le {apologyInformation.createdAt}
               </p>
-              {apologyInformation.categories.map((categorie) => (
-                <div className="apology-categorie">
-                  {categorie.title}
-                </div>
-              ))}
               <div className="apology-fame">
-                <ThumbsUp />
+                <button
+                  type="button"
+                  className="comment-button"
+                >
+                  <MessageSquare
+                    className="comment-icon"
+                  />
+                </button>
+                <button
+                  type="button"
+                  className="like-button"
+                >
+                  <ThumbsUp
+                    className="like-icon"
+                  />
+                </button>
+
                 {apologyInformation.likes}
               </div>
             </div>
+            <div className="apology-categories">
+              {apologyInformation.categories.map((categorie) => (
+                <div key={categorie.title} className="categories-item">
+                  {categorie.title}
+                </div>
+              ))}
+            </div>
+            <hr />
             <div className="apology-commentary">
+              <h3 className="comments-title">
+                Commentaires :
+              </h3>
               {apologyInformation.comments.map((comment) => (
-                <>
+                <div key={comment.id} className="comment">
                   <div className="comment-item">
                     {comment.content}
                   </div>
                   <div className="comment-infos">
                     {comment.author.nickname}
                   </div>
-                </>
+                </div>
               ))}
             </div>
           </section>
